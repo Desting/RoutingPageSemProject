02909ba7cf5e7190d262b8237454eb8c387422da
Desting <Nik_zd@hotmail.com>
2014-11-28 13:58:29 +0100

    Initial Commit

---
+++ public/app/components/controllers.js
@@ -0,0 +1,10 @@
+angular.module('myAppRename.controllers', []).
+  controller('AppCtrl', function ($scope) {
+    $scope.title = "BoNiPe Distribution";
+  })
+  .controller('MyCtrl2', function ($scope) {
+    // write MyCtrl2 here
+  });
+
+
+
